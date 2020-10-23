ba6037a988cd3b062c7c4a23c4848e2b6e0712e5
el-tocino <email+github@aforkinthetoad.com>
2020-10-23 00:05:48 -0500

    Update display.py

--- display.py
+++ display.py
@@ -28,7 +28,7 @@ def set_bgcolor(bgcolor):
 
 def add_text(text_string, text_color):
     """ write the text to display. """
-    font = pygame.font.Font(None, 80)
+    font = pygame.font.Font(None, 55)
     text = font.render(text_string, True, text_color)
     text_rect = text.get_rect(center=(400, 240))
     screen.blit(text, text_rect)
