827150af392d4be5ef84e3c23b50668c846ca2e0
el-tocino <email+github@aforkinthetoad.com>
2020-10-31 01:11:20 -0500

    Update display.py

--- display.py
+++ display.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """ show the distance to an object, and a relevant background color. """
 
 from time import sleep
@@ -5,14 +6,15 @@ import pygame
 from gpiozero import DistanceSensor
 
 # Initialize ultrasonic sensor
-sensor = DistanceSensor(trigger=18, echo=24)
+sensor = DistanceSensor(trigger=24, echo=23)
 
 # Init pygame
 pygame.init()
 #pi 5" display
-screen = pygame.display.set_mode([800, 480])
-# testing screen
-##screen = pygame.display.set_mode([1920, 1080])
+screentraits = pygame.display.Info()
+wres = screentraits.current_w
+hres = screentraits.current_h
+screen = pygame.display.set_mode([wres, hres])
 white = [255, 255, 255]
 red = [255, 0, 0]
 blue = [0, 0, 255]
@@ -26,37 +28,51 @@ def set_bgcolor(bgcolor):
     screen.fill(bgcolor)
     pygame.display.update()
 
-def add_text(text_string, text_color):
+
+def add_distance(dist, text_color):
+    """ add distanceto display. """
+    font = pygame.font.SysFont(None, 60)
+    text = font.render(dist, True, text_color)
+    text_rect = text.get_rect(center=(int(wres / 2), int(hres / 2 + 100 )))
+    screen.blit(text, text_rect)
+    pygame.display.update()
+
+def add_text(text_string, text_color, fontsize):
     """ write the text to display. """
-    font = pygame.font.Font(None, 55)
+    font = pygame.font.Font(None, fontsize)
     text = font.render(text_string, True, text_color)
-    text_rect = text.get_rect(center=(400, 240))
+    text_rect = text.get_rect(center=((wres / 2), (hres / 2) - 100))
     screen.blit(text, text_rect)
     pygame.display.update()
 
 def show_distance(distance):
     """ set the bg color, then print the distance if it's under 2 meters. """
     distnc = float(distance)
-    if distnc < .9:
+    if distnc < .4:
         new_text = "Won't you back that ass up?"
-        set_bgcolor(black)
-        tcolor = white
-        # Juvenile
-    elif distnc < 1.5:
-        new_text = "GET BACK, MOTHER FUCKER YOU DON'T KNOW ME LIKE THAT!"
         set_bgcolor(red)
-        tcolor = yellow
-        # Ludacris
-    elif distnc < 2.1:
-        new_text = "Don't stand so close to me!"
+        tcolor = yellow 
+        fsize = 64
+        # Juvenile
+    elif distnc < 1.0:
+        new_text = "GET BACK, MOTHER FUCKER YOU DON'T KNOW ME LIKE THAT!"
         set_bgcolor(yellow)
-        tcolor = blue
+        tcolor = blue 
+        fsize = 52
+        # Ludacris
+    elif distnc < 2:
+        new_text = "Don't stand so close to me!"
+        set_bgcolor(black)
+        tcolor = white 
+        fsize = 70
         # The Police
     else:
         set_bgcolor(blue)
-    add_text(new_text, tcolor)
+    add_text(new_text, tcolor, fsize)
+    add_distance(distnc, tcolor)
 
 while True:
-    sleep(2)
+    sleep(1)
     getdist = sensor.distance
+    print (getdist)
     show_distance(getdist)
